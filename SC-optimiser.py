629372e4d86d9891875bd34378e7e21c09233b5d
Joe-Egan-22 <joseph.egan22@icloud.com>
2026-07-29 15:53:35 +0100

    Created functions to: Read product data, test individual functions as they are created (rather than forming a main() straight away and have started to create a function to model the lp problem with PuLP

--- SC-optimiser.py
+++ SC-optimiser.py
@@ -2,3 +2,44 @@ import pulp
 import pandas as pd
 import numpy as np
 
+# Product database input in csv format
+PRODUCT_FILE = 'Databases/Finishedproducts.csv'
+
+
+def read_data():
+    '''
+    Create pandas dataframes from csv files in directory
+    '''
+    # Create dataframes
+    product_df = pd.read_csv(PRODUCT_FILE, delimiter=',', index_col='ProductID')
+
+    # Access desired columns from dataframe
+    profits = product_df['ProfitPerUnit']
+    variable_names = product_df['ProductName']
+
+    return profits, variable_names
+
+def lp_model():
+    '''
+    Creates the LP model
+    '''
+
+    # define lp model
+    model = pulp.LpProblem('Profit Maximisation Problem', pulp.LpMaximize)
+
+
+    return model
+
+def test_function():
+    '''
+    Function to test outputs of individual functions.
+    '''
+    data = read_data()
+
+    obj_coef_array = np.array(data[0])
+
+    return print(obj_coef_array)
+
+
+
+test_function()
