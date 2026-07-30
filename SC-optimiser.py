61567c08c505424ec58ee5680149df6ec95e7ab6
Joe-Egan-22 <joseph.egan22@icloud.com>
2026-07-30 12:22:49 +0100

    Accessed constraint coefficients

--- SC-optimiser.py
+++ SC-optimiser.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Product database input in csv format
 PRODUCT_FILE = 'Databases/Finishedproducts.csv'
-REQUIREMENT_FILE = 'Databases/BOMs.csv'
+BOM_FILE = 'Databases/BOMs.csv'
 
 '''
 FOLLOWING GLOBAL VARIABLES DECIDED BASED ON INTERNET SEARCH, 
@@ -21,7 +21,7 @@ def read_data():
     '''
     # Create dataframes
     product_df = pd.read_csv(PRODUCT_FILE, delimiter=',', index_col='ProductID')
-    requirement_df = pd.read_csv(REQUIREMENT_FILE, delimiter=',', index_col='MaterialID')
+    bom_df = pd.read_csv(BOM_FILE, delimiter=',', index_col='MaterialID')
 
     # Access desired columns from dataframes
     profits = product_df['ProfitPerUnit']
@@ -29,19 +29,19 @@ def read_data():
     machine_hours = product_df['MachineHours']
     labour_hours = product_df['LabourHours']
 
-    # Obtain constraint coefficients
-    mat_reqs = requirement_df.sort_index()
 
-    for i in mat_reqs:
-        for j in mat_reqs['ProductID']:
-            coeff_i = mat_reqs[i] == j
+    # Pivot bom table to more convenient format
+    bom_piv = (bom_df.pivot_table(index='MaterialID', columns='ProductID',values='QuantityRequired',fill_value=0))
+
+    # Access coefficients of material constraints
+    mat_coefs = bom_piv
 
     '''
     mat_filt_1 = mat_reqs["ProductID"] == 'FP001'
     mats_for_1 = mat_reqs[mat_filt_1]
     '''
 
-    return profits, variable_names, machine_hours, labour_hours, mats_for_1
+    return profits, variable_names, machine_hours, labour_hours, mat_coefs
 
 def lp_model(obj_coefs, dec_vars, const_coefs):
     '''
@@ -64,9 +64,9 @@ def test_function():
 
     obj_coef_array = np.array(data[0])
     var_names = data[1].to_dict
-    constraint_matrix = []
+    constraint_matrix = data[4]
 
-    #lp_model(obj_coef_array, var_names, constraint_matrix)
+    lp_model(obj_coef_array, var_names, constraint_matrix)
 
     return print(data[4])
 
