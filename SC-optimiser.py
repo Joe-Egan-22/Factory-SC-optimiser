b6e326ae0b09596f7f984f3da834caaa66230960
Joe-Egan-22 <joseph.egan22@icloud.com>
2026-08-17 18:00:04 +0100

    Alterred structure to have create_lp_model just creating model and have additional functions to form objective function and apply material constrains

--- SC-optimiser.py
+++ SC-optimiser.py
@@ -164,24 +164,27 @@ def create_lp_model(data):
     '''
     Creates the LP model
     '''
-    
-    # LHS Coefficients
-    objective_coeffs = data["Products"]['ProfitPerUnit']
-    mat_constraint_coeffs = data["BOM"]
-    time_constraint_coeffs = data['Products'][['MachineHours', 'LabourHours']].transpose()
-
-    # RHS Coefficients
-    mat_rhs = data['Inventory']['QuantityInStock']
-    time_rhs = data['AvailableTime'] # Need to have these in CSV
-
-    # Names
+   
     decision_var_names = data["Products"].index
 
     # Define lp model
     model = pulp.LpProblem('Profit_Maximisation_Problem', pulp.LpMaximize)
 
-    # Create variable names
+    # Create decision variable
     X = pulp.LpVariable.dicts('Prod', decision_var_names, lowBound=0, cat='Continuous')
+
+    objective_function(model, data, X)
+    material_constraints(model, data, X)
+    time_constrains(model, data, X)
+
+    return model
+
+def objective_function(model, data, X):
+    '''
+    Creates the objective function for LP problem
+    '''
+
+    objective_coeffs = data["Products"]['ProfitPerUnit']
 
     # Create linear expression from objective coefficients
     model += pulp.lpSum(
@@ -189,28 +192,48 @@ def create_lp_model(data):
         for p in objective_coeffs.index
     )
 
+    return model
+
+def material_constraints(model, data, X):
+    '''
+    Applies material constraints to LP model
+    '''
+
+    mat_constraint_coeffs = data["BOM"]
+    mat_rhs = data['Inventory']['QuantityInStock']
+
     # Using lpSum to create linear expression for material constraints
     for material in mat_constraint_coeffs.index:
 
         model += (
             pulp.lpSum(
                 mat_constraint_coeffs.loc[material, product] * X[product]
-                for product in decision_var_names
+                for product in data["Products"].index
             )
             <= mat_rhs[material],
             material
         )
 
+    return model
+
+def time_constrains(model, data, X):
+    '''
+    Applies time constraints to LP model
+    '''
+
+    time_constraint_coeffs = data['Products'][['MachineHours', 'LabourHours']].transpose()
+    time_rhs = data['AvailableTime'] # Need to have these in CSV
+
     for constr in time_constraint_coeffs.index: 
 
-        model += (
-            pulp.lpSum(
-                time_constraint_coeffs.loc[constr, prod] * X[prod]
-                for prod in decision_var_names
+            model += (
+                pulp.lpSum(
+                    time_constraint_coeffs.loc[constr, prod] * X[prod]
+                    for prod in data["Products"].index
+                )
+                <= time_rhs[constr],
+                constr
             )
-            <= time_rhs[constr],
-            constr
-        )
 
     return model
 
